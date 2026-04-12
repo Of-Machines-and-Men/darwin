d4816745de4358de595d32208c441245f9cd1e85
ItsHolmesy <matt@yellowtwist.net>
2026-04-12 23:02:46 +1000

    Point system

--- source/scenes/UI/spawn toolbar/subcomponents/spawn_toolbar_panel.gd
+++ source/scenes/UI/spawn toolbar/subcomponents/spawn_toolbar_panel.gd
@@ -13,7 +13,7 @@ func set_value(new_value: GoonDefinition) -> void:
 	value = new_value
 	disabled = false
 	icon_container.texture = new_value.icon
-	cost_label.text = "%f.0" % new_value.spawn_cost
+	cost_label.text = str(new_value.get_effective_spawn_cost())
 	name_label.text = new_value.display_name
 
 func _handle_pressed() -> void:
