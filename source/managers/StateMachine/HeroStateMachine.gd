952171f6740e9d3914481f7c0f45975bc9659591
ItsHolmesy <matt@yellowtwist.net>
2026-04-15 01:35:22 +1000

    More visual changes

--- source/managers/StateMachine/HeroStateMachine.gd
+++ source/managers/StateMachine/HeroStateMachine.gd
@@ -1,5 +1,20 @@
 class_name HeroStateMachine
 extends StateMachine
 
+const IDLE_TEX: Texture2D = preload("res://assets/art/hero/heroIdle.png")
+const MOVE_TEX: Texture2D = preload("res://assets/art/hero/heroMove.png")
+const BACKWARD_TEX: Texture2D = preload("res://assets/art/hero/heroBackward.png")
+const LIGHTJAB_TEX: Texture2D = preload("res://assets/art/hero/heroLightjab.png")
+const SUPERPUNCH1_TEX: Texture2D = preload("res://assets/art/hero/heroSuperpunch1.png")
+const SUPERPUNCH2_TEX: Texture2D = preload("res://assets/art/hero/heroSuperpunch2.png")
+const HURT_TEX: Texture2D = preload("res://assets/art/hero/heroPain.png")
+const DEAD_TEX: Texture2D = preload("res://assets/art/hero/heroDead.png")
+const DEAD2_TEX: Texture2D = preload("res://assets/art/hero/heroDead2.png")
+
+@onready var sprite: Sprite2D = $"../Sprite2D"
+
 func _ready() -> void:
 	current_state = IdleState.new()
+
+func play(texture: Texture2D) -> void:
+	sprite.texture = texture
---
+++ source/scenes/levels/simple_arena.gd
@@ -0,0 +1,60 @@
+extends Node2D
+
+const GOON_SCENE: PackedScene = preload("res://source/entities/goon/goon.tscn")
+const LAB_MENU_SCENE: PackedScene = preload("res://source/scenes/UI/LabMenu.tscn")
+
+@onready var hero: EntityBase = $GameplayLayer/Hero
+@onready var gameplay_layer: Node = $GameplayLayer
+@onready var ui_layer: CanvasLayer = $UILayer
+
+var _goon_spawn_positions: Array[Vector2] = []
+var _live_goons: Array[EntityBase] = []
+var _lab_menu: LabMenu = null
+var _upgrade_database: UpgradeDatabase
+var _chosen_upgrades: Array[UpgradeDefinition] = []
+
+func _ready() -> void:
+	_upgrade_database = DefaultUpgradeDatabase.new()
+	MutiePointsManager.register_hero(hero.health)
+
+	for child in gameplay_layer.get_children():
+		if child is EntityBase and child != hero:
+			_goon_spawn_positions.append(child.global_position)
+			_track_goon(child)
+
+func _track_goon(goon: EntityBase) -> void:
+	_live_goons.append(goon)
+	goon.health.on_death.connect(_on_goon_died.bind(goon))
+
+func _on_goon_died(goon: EntityBase) -> void:
+	_live_goons.erase(goon)
+	if _live_goons.is_empty():
+		_end_round()
+
+func _end_round() -> void:
+	hero.process_mode = Node.PROCESS_MODE_DISABLED
+	_open_lab_menu()
+
+func _open_lab_menu() -> void:
+	_lab_menu = LAB_MENU_SCENE.instantiate()
+	_lab_menu.upgrade_database = _upgrade_database
+	ui_layer.add_child(_lab_menu)
+	_lab_menu.upgrade_chosen.connect(_on_upgrade_chosen)
+
+func _on_upgrade_chosen(definition: UpgradeDefinition) -> void:
+	_chosen_upgrades.append(definition)
+	if _lab_menu:
+		_lab_menu.queue_free()
+		_lab_menu = null
+	hero.process_mode = Node.PROCESS_MODE_INHERIT
+	_spawn_round()
+
+func _spawn_round() -> void:
+	_live_goons.clear()
+	for pos in _goon_spawn_positions:
+		var goon: EntityBase = GOON_SCENE.instantiate()
+		gameplay_layer.add_child(goon)
+		goon.global_position = pos
+		for upgrade in _chosen_upgrades:
+			upgrade.apply_to(goon)
+		_track_goon(goon)
